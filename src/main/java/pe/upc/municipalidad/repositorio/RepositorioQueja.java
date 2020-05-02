fe347ec9d7162d7786998eb06986381fb39e8784
Freddy Alberto Bravo Angulo <u201300062@upc.edu.pe>
2020-05-02 04:25:44 -0500

    registrando quejas

--- src/main/java/pe/upc/municipalidad/repositorio/RepositorioQueja.java
+++ src/main/java/pe/upc/municipalidad/repositorio/RepositorioQueja.java
@@ -8,6 +8,5 @@ import pe.upc.municipalidad.entidades.Queja;
 import java.util.List;
 
 public interface RepositorioQueja extends JpaRepository<Queja,Long> {
-    @Query("select a from Queja a where a.dni=:dni")
-    public List<Queja> obtenerQueja(@Param("dni") int dni);
+
 }
