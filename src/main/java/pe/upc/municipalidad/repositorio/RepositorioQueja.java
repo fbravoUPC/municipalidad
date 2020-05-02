878088e8b451cb574284a4819489599f6ec0b49d
Freddy Alberto Bravo Angulo <u201300062@upc.edu.pe>
2020-05-02 04:37:26 -0500

    registrando quejas1

--- src/main/java/pe/upc/municipalidad/repositorio/RepositorioQueja.java
+++ src/main/java/pe/upc/municipalidad/repositorio/RepositorioQueja.java
@@ -1,11 +1,10 @@
 package pe.upc.municipalidad.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import pe.upc.municipalidad.entidades.Queja;
 
-import java.util.List;
+
 
 public interface RepositorioQueja extends JpaRepository<Queja,Long> {
 
