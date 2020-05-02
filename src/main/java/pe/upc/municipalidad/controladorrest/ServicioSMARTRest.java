98aa05e40f09d8348fb2c1c24277d07952303832
Freddy Alberto Bravo Angulo <u201300062@upc.edu.pe>
2020-05-02 04:55:38 -0500

    queja terminado

--- src/main/java/pe/upc/municipalidad/controladorrest/ServicioSMARTRest.java
+++ src/main/java/pe/upc/municipalidad/controladorrest/ServicioSMARTRest.java
@@ -1,12 +1,17 @@
 package pe.upc.municipalidad.controladorrest;
 
+import jdk.nashorn.internal.ir.Assignment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import pe.upc.municipalidad.entidades.EstadoQueja;
 import pe.upc.municipalidad.entidades.Queja;
+import pe.upc.municipalidad.entidades.TipoQueja;
+import pe.upc.municipalidad.entidades.Vecino;
 import pe.upc.municipalidad.servicios.ServiciosSmartQueja;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -14,23 +19,73 @@ import java.util.List;
 public class ServicioSMARTRest {
     @Autowired
     private ServiciosSmartQueja serviciosSmartQueja;
-    @PostMapping("muni/queja/registrar")
-    public Queja registrar(@RequestBody Queja queja)
+
+    //-------------------------------------------------------------------------------
+    //Vecino
+    @PostMapping("registrar/vecino")
+    public Vecino registrar(@RequestBody Vecino vecino)
     {
-        Queja q;
+        Vecino v;
         try{
-            q=serviciosSmartQueja.registrar(queja);
+            v=serviciosSmartQueja.registrarVecino(vecino);
         }catch (Exception e){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No se puede registrar ",e);
         }
-        return q;
+        return v;
     }
-    @GetMapping("muni/queja/{dni}")
-    public List<Queja> obtenerQueja(@PathVariable("dni")int dni) {
+
+    //-------------------------------------------------------------------------------
+    //TipoQueja
+    @PostMapping("registrar/tipoqueja")
+    public TipoQueja registrar(@RequestBody TipoQueja tipoQueja)
+    {
+        TipoQueja tq;
+        try{
+            tq=serviciosSmartQueja.registrarTipoQueja(tipoQueja);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No se puede registrar ",e);
+        }
+        return tq;
+    }
+
+    //-------------------------------------------------------------------------------
+    //EstadoQueja
+    @PostMapping("registrar/estadoqueja")
+    public EstadoQueja registrar(@RequestBody EstadoQueja estadoQueja)
+    {
+        EstadoQueja eq;
+        try{
+            eq=serviciosSmartQueja.registrarEstadoQueja(estadoQueja);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No se puede registrar ",e);
+        }
+        return eq;
+    }
+
+
+    //-------------------------------------------------------------------------------
+    //Queja
+    @PostMapping("registrar/estado/{estadoid}/tipo/{tipoid}/vecino/{dni}/queja")
+    public Queja registrar(@PathVariable(value = "estadoid") Long estadoid,
+                           @PathVariable(value = "tipoid") Long tipoid,
+                           @PathVariable(value = "dni") String dni,
+                           @RequestBody Queja queja)
+    {
+        return serviciosSmartQueja.registrarQueja(estadoid,tipoid,dni,queja);
+    }
+
+    @GetMapping("obtenerQueja/{dni}")
+    public List<Queja> obtenerQueja(@PathVariable("dni")String dni) {
         try {
-            return serviciosSmartQueja.obtenerQueja(dni);
+            return serviciosSmartQueja.obtenerQuejasVecinoDNI(dni);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No existe producto", e);
-        }   
+        }
+
+    }
+
+    @GetMapping("/quejas")
+    public List<Queja> getAllUsers() {
+        return serviciosSmartQueja.obtenerQuejas();
     }
 }
